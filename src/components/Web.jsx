c7d8db0133cee3360f8403d7209101ca70f57c46
Madagoni Vinay Goud <madagonivinay@gmail.com>
2026-08-08 19:02:06 +0530

    Uncomment background div in Web component

--- src/components/Web.jsx
+++ src/components/Web.jsx
@@ -87,7 +87,7 @@ export default function Web() {
           }}
         />
         {/* light uniform darken, not black gradient */}
-            {/* <div className="absolute inset-0" style={{ background: "rgba(255,255,255,0.25)"}} /> */}
+             <div className="absolute inset-0" style={{ background: "rgba(255,255,255,0.25)"}} />
 
         
       </div>
